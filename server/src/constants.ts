52391594fe84da3b2f847bbdf104d1838b55b111
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2024-03-24 17:43:22 +0530

    change cookie life to 1 day

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -8,8 +8,7 @@ export const DATA_MODEL_KEYS = {
 
 export const AUTH_COOKIE_KEY = 'auth-token';
 export const cookieOptions: CookieOptions = {
-  // Not creating a refresh token logic, using same token as refresh token
-  maxAge: 20 * 60 * 1000,
+  maxAge: 24 * 60 * 60 * 1000,
   sameSite: 'none',
   httpOnly: true,
   secure: true
